449a7893d0d73b2539ca9c189e2e259d768813f1
Olga Naboichenko <olga140404ol@gmail.com>
2022-05-19 14:48:54 +0300

    home-task -2

---
+++ index.js
@@ -0,0 +1,6 @@
+const question1 = 'Введите первое число';
+const a = parseInt(prompt(question1));
+const question2 = 'Введите второе число';
+const b = parseInt(prompt(question2));
+var c = a + b;
+alert(c);
